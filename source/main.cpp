d1a1ac88f5fb876a88db47b9895141dea01be430
Tom Flaherty <tom.flaherty03@gmail.com>
2024-01-11 18:47:24 +0000

    Updated the preprocessor checks for oled.hpp settings

--- source/main.cpp
+++ source/main.cpp
@@ -14,8 +14,13 @@
 #include "settings.hpp"
 #include "global.hpp"
 
-#if defined(OLED_INCLUDE_FONT8) || defined(OLED_INCLUDE_FONT16) || defined(OLED_INCLUDE_FONT20) || !defined(OLED_INCLUDE_FONT12) || !defined(OLED_INCLUDE_FONT24) || defined(OLED_INCLUDE_TEST_FUNCTION) || !defined(OLED_INCLUDE_LOADING_CIRCLE) || !defined(OLED_INCLUDE_LOADING_BAR_HORIZONTAL) || !defined(OLED_INCLUDE_SD_IMAGES) || !defined(OLED_INCLUDE_QR_GENERATOR)
-#error "OLED settings includes unnecessary font tables or functions, or does not include required font tables"
+// Check that only the necessary parts of the oled code have been included. This saves several kB of RAM
+// Parts of oled.hpp that should have been included
+#if !defined(OLED_INCLUDE_FONT12) || !defined(OLED_INCLUDE_FONT24)  || !defined(OLED_INCLUDE_LOADING_CIRCLE) || !defined(OLED_INCLUDE_LOADING_BAR_HORIZONTAL) || !defined(OLED_INCLUDE_SD_IMAGES) || !defined(OLED_INCLUDE_QR_GENERATOR)
+#error "A required part of oled.hpp has not been included"
+// Parts of oled.hpp that should not have been included
+#elif defined(OLED_INCLUDE_FONT8) || defined(OLED_INCLUDE_FONT16) || defined(OLED_INCLUDE_FONT20) || defined(OLED_INCLUDE_TEST_FUNCTION)
+#error "An unnecessary part of oled.hpp has been included, this can waste lots of RAM or program space"
 #endif
 
 /* --- HARDWARE DEFINITIONS --- */
